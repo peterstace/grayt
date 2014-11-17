59ebcae6881c2d9665ce0301c3b9ff4cae5cbe03
Peter Stace <peterstace@gmail.com>
2014-11-17 21:00:13 +1100

    Add tracer struct

--- examples/plane_and_spheres/main.go
+++ examples/plane_and_spheres/main.go
@@ -3,8 +3,10 @@ package main
 import "github.com/peterstace/grayt"
 
 func main() {
+
+	tracer := grayt.NewTracer()
 	sceneFactory := func(t float64) grayt.Scene {
 		return grayt.Scene{}
 	}
-	grayt.TraceAnimation("output", sceneFactory)
+	tracer.Trace("output", sceneFactory)
 }
--- tracer.go
+++ tracer.go
@@ -1,5 +1,26 @@
 package grayt
 
-func TraceAnimation(path string, sceneFactory func(t float64) Scene) error {
+import "log"
+
+type Tracer struct {
+	numFrames       int
+	samplesPerFrame int
+}
+
+func NewTracer() Tracer {
+	return Tracer{
+		numFrames:       1,
+		samplesPerFrame: 1,
+	}
+}
+
+func (t *Tracer) SetSamplesPerFrame(samplesPerFrame int) {
+	t.samplesPerFrame = samplesPerFrame
+}
+
+func (t *Tracer) Trace(path string, sceneFactory func(t float64) Scene) error {
+
+	log.Print("Tracing Animation...")
+
 	return nil
 }
