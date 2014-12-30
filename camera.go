978b79394c5c93dc9eb98c32a2bd648b6a73a28c
Peter Stace <peterstace@gmail.com>
2014-12-30 22:14:11 +1100

    Add coordinate system comment to the Camera interface

--- camera.go
+++ camera.go
@@ -1,5 +1,11 @@
 package grayt
 
+// Cameras produce rays that go from an eye to a virtual screen. The rays
+// produced are specified via a coordiate system on the virtual screen.  The
+// left side of the virtual screen has x coordinate -1, the right side of the
+// virtual screen has coordinate +1. The top of the virtual screen has y
+// coordinate v and the bottom of the virtual screen has y coordinate -v (where
+// the value of v depends on the aspect ratio of the screen).
 type Camera interface {
 	MakeRay(x, y float64) ray
 }
@@ -10,7 +16,7 @@ type RectilinearCamera struct {
 	eyeLoc           Vect
 }
 
-func NewRectilinearCamera() *RectilinearCamera {
+func NewRectilinearCamera() Camera {
 	return nil
 }
 
