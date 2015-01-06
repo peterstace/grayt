a00a9d47853d1799a850d5b5e2cf903c247b6f2b
Peter Stace <peterstace@gmail.com>
2015-01-06 21:04:04 +1100

    Add `Surface` interface

--- obj.go
+++ obj.go
@@ -1,4 +1,10 @@
 package grayt
 
-type Obj interface {
+type Surface interface {
+
+	// HitSurface checks for a surface hit. If the surface is in front, the
+	// first return value is the ray multiplier required to hit the surface. If
+	// the surface is behind or cannot be hit, the first return value is
+	// negative. The second return value is the unit normal at the surface hit.
+	HitSurface(ray) (float64, Vect)
 }
