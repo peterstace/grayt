cd66bf646b54c0c89861b52405c5f4e7a96dbff4
Peter Stace <peterstace@gmail.com>
2015-04-15 08:16:59 +1000

    Don't create mkv inside tracer. Get rid of premature factory abstraction.

--- image_tracer/main.go
+++ image_tracer/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/peterstace/grayt/tracer"
 	"github.com/peterstace/grayt/vect"
@@ -11,34 +10,37 @@ import (
 func main() {
 
 	mov := tracer.Movie{
-		Duration: time.Second * 17,
-		Camera: tracer.StillCamera(tracer.NewRectilinearCamera(
-			tracer.CameraConfig{
-				Location:      vect.New(0, 10, 5),
-				ViewDirection: vect.New(0, -1, -2),
-				UpDirection:   vect.New(0, 1, 0),
-				FieldOfView:   1.5,
-				FocalLength:   10.0,
-				FocalRatio:    1000.0,
-			})),
-		Geometries: []tracer.GeometryFn{
-
-			tracer.StillGeometry(tracer.NewPlane(vect.New(0, 0, 0), vect.New(0, 1, 0))),
-
-			tracer.StillGeometry(tracer.NewPlane(vect.New(0, -0.5, -10), vect.New(0.2, 1, 0))),
-			tracer.StillGeometry(tracer.NewPlane(vect.New(0, -0.5, -10), vect.New(-0.2, 1, 0))),
-			tracer.StillGeometry(tracer.NewPlane(vect.New(0, -0.5, -10), vect.New(0, 1, 0.2))),
-			tracer.StillGeometry(tracer.NewPlane(vect.New(0, -0.5, -10), vect.New(0, 1, -0.2))),
-
-			tracer.StillGeometry(tracer.NewSphere(vect.New(0, 1, -10), 1)),
+		FrameCount: 512,
+		CameraFactory: func(t float64) tracer.Camera {
+			return tracer.NewRectilinearCamera(
+				tracer.CameraConfig{
+					Location:      vect.New(0, 10, 5),
+					ViewDirection: vect.New(0, -1, -2),
+					UpDirection:   vect.New(0, 1, 0),
+					FieldOfView:   1.5,
+					FocalLength:   10.0,
+					FocalRatio:    1000.0,
+				},
+			)
 		},
-
-		Lights: []tracer.LightFn{
-			tracer.StillLight(tracer.Light{Location: vect.New(0, 10, -10), Radius: 0.5, Intensity: 100}),
+		GeometriesFactory: func(t float64) []tracer.Geometry {
+			return []tracer.Geometry{
+				tracer.NewPlane(vect.New(0, 0, 0), vect.New(0, 1, 0)),
+				tracer.NewPlane(vect.New(0, -0.5, -10), vect.New(0.2, 1, 0)),
+				tracer.NewPlane(vect.New(0, -0.5, -10), vect.New(-0.2, 1, 0)),
+				tracer.NewPlane(vect.New(0, -0.5, -10), vect.New(0, 1, 0.2)),
+				tracer.NewPlane(vect.New(0, -0.5, -10), vect.New(0, 1, -0.2)),
+				tracer.NewSphere(vect.New(0, 1, -10), 1),
+			}
+		},
+		LightsFactory: func(t float64) []tracer.Light {
+			return []tracer.Light{
+				tracer.Light{Location: vect.New(0, 10, -10), Radius: 0.5, Intensity: 100},
+			}
 		},
 	}
 
-	if err := tracer.TraceMovie(mov, "out.mkv"); err != nil {
+	if err := tracer.TraceMovie(mov, "out"); err != nil {
 		log.Fatal(err)
 	}
 }
--- tracer/animation.go
+++ tracer/animation.go
@@ -2,108 +2,89 @@ package tracer
 
 import (
 	"fmt"
-	"image"
 	"image/jpeg"
-	"io/ioutil"
-	"log"
 	"math/rand"
 	"os"
-	"os/exec"
 	"path"
-	"time"
 )
 
-type CameraFn func(float64) Camera
-type GeometryFn func(float64) Geometry
-type LightFn func(float64) Light
-
-func StillCamera(c Camera) CameraFn {
-	return func(float64) Camera { return c }
-}
-func StillGeometry(g Geometry) GeometryFn {
-	return func(float64) Geometry { return g }
-}
-func StillLight(l Light) LightFn {
-	return func(float64) Light { return l }
-}
-
 type Movie struct {
-	Duration   time.Duration
-	Camera     CameraFn
-	Geometries []GeometryFn
-	Lights     []LightFn
+	FrameCount        int
+	CameraFactory     func(t float64) Camera
+	GeometriesFactory func(t float64) []Geometry
+	LightsFactory     func(t float64) []Light
 }
 
-func TraceMovie(m Movie, filename string) error {
+func TraceMovie(m Movie, outputDir string) error {
 
-	// Calculate the number of frames.
-	const fps = 24
-	numFrames := int(m.Duration.Seconds() * fps)
-
-	imgs := make([]image.Image, numFrames)
-	for i := 0; i < numFrames; i++ {
+	for i := 0; i < m.FrameCount; i++ {
 
 		// Create the sample(s).
-		t := Sample(i, numFrames)
-		var sample Scene
-		sample.Camera = m.Camera(t)
-		for _, g := range m.Geometries {
-			sample.Geometries = append(sample.Geometries, g(t))
-		}
-		for _, l := range m.Lights {
-			sample.Lights = append(sample.Lights, l(t))
+		// TODO: This currently only creates a single sample per frame. It
+		// should instead generate multiple samples per frame.
+		t := Sample(i, m.FrameCount)
+		sample := Scene{
+			Camera:     m.CameraFactory(t),
+			Geometries: m.GeometriesFactory(t),
+			Lights:     m.LightsFactory(t),
 		}
 
 		// Trace the image.
-		imgs[i] = TraceImage([]Scene{sample})
+		img := TraceImage([]Scene{sample})
 
-	}
-
-	return outputMovie(imgs, fps, filename)
-}
-
-func outputMovie(imgs []image.Image, fps int, filename string) error {
-
-	log.Print("outputting movie")
-
-	// Get a temp dir to put the JPEG for each frame into.
-	tmpDir, err := ioutil.TempDir("", "")
-	if err != nil {
-		return err
-	}
-	defer func() {
-		log.Printf("cleaning up %q", tmpDir)
-		os.RemoveAll(tmpDir)
-	}()
-
-	// Write the images out to file.
-	for i := 0; i < len(imgs); i++ {
-		if out, err := os.Create(path.Join(tmpDir, fmt.Sprintf("%d.jpg", i))); err != nil {
+		// Output the image.
+		// TODO: Should pad filename with leading zeros.
+		if file, err := os.Create(path.Join(outputDir, fmt.Sprintf("%d.jpg", i))); err != nil {
 			return err
-		} else if err := jpeg.Encode(out, imgs[i], nil); err != nil {
+		} else if err := jpeg.Encode(file, img, nil); err != nil {
 			return err
 		}
 	}
-
-	// Create a movie file from the images.
-	wd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	cmd := exec.Command("ffmpeg",
-		"-framerate", fmt.Sprintf("%d", fps),
-		"-i", "%d.jpg",
-		"-codec", "copy",
-		path.Join(wd, filename))
-	cmd.Dir = tmpDir
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("%s: %s", err, string(out))
-	}
-
-	log.Printf("movie output to %q", filename)
 	return nil
 }
+
+//func outputMovie(imgs []image.Image, fps int, filename string) error {
+//
+//	log.Print("outputting movie")
+//
+//	// Get a temp dir to put the JPEG for each frame into.
+//	tmpDir, err := ioutil.TempDir("", "")
+//	if err != nil {
+//		return err
+//	}
+//	defer func() {
+//		log.Printf("cleaning up %q", tmpDir)
+//		os.RemoveAll(tmpDir)
+//	}()
+//
+//	// Write the images out to file.
+//	for i := 0; i < len(imgs); i++ {
+//		if out, err := os.Create(path.Join(tmpDir, fmt.Sprintf("%d.jpg", i))); err != nil {
+//			return err
+//		} else if err := jpeg.Encode(out, imgs[i], nil); err != nil {
+//			return err
+//		}
+//	}
+//
+//	// Create a movie file from the images.
+//	wd, err := os.Getwd()
+//	if err != nil {
+//		return err
+//	}
+//	cmd := exec.Command("ffmpeg",
+//		"-framerate", fmt.Sprintf("%d", fps),
+//		"-i", "%d.jpg",
+//		"-codec", "copy",
+//		path.Join(wd, filename))
+//	cmd.Dir = tmpDir
+//	out, err := cmd.CombinedOutput()
+//	if err != nil {
+//		return fmt.Errorf("%s: %s", err, string(out))
+//	}
+//
+//	log.Printf("movie output to %q", filename)
+//	return nil
+//}
 
 // Sample returns a value in the interval [0, 1). The interval [0, 1) is
 // divided equally into n parts, with the random value being selected from
