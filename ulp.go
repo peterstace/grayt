7f96f813cc59883bf70d81b8f8695e9697d91270
Peter Stace <peterstace@gmail.com>
2015-05-14 20:17:59 +1000

    Remove custom float64ToULP function

--- ulp.go
+++ ulp.go
@@ -1,19 +1,15 @@
 package grayt
 
-import "unsafe"
+import "math"
 
 func ulpDiff(a, b float64) uint64 {
 
-	ulpA := float64ToULP(a)
-	ulpB := float64ToULP(b)
+	ulpA := math.Float64bits(a)
+	ulpB := math.Float64bits(b)
 
 	if ulpA > ulpB {
 		return ulpA - ulpB
 	} else {
 		return ulpB - ulpA
 	}
-}
-
-func float64ToULP(f float64) uint64 {
-	return *(*uint64)(unsafe.Pointer(&f))
 }
