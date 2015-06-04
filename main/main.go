2a2dc093e4c2a0c79a787440a8dbd85c0312639c
Peter Stace <peterstace@gmail.com>
2015-06-04 21:27:43 +1000

    Add samples per pixel as a command line option

--- main/main.go
+++ main/main.go
@@ -12,9 +12,13 @@ import "flag"
 
 func main() {
 
-	var out string
+	var (
+		out string
+		spp int
+	)
 
 	flag.StringVar(&out, "o", "", "output file (must end in .png)")
+	flag.IntVar(&spp, "s", 10, "samples per pixel")
 	flag.Parse()
 
 	if !strings.HasSuffix(out, ".png") {
@@ -25,7 +29,7 @@ func main() {
 	cam := CornellBoxCamera()
 
 	acc := grayt.NewAccumulator(300, 300)
-	grayt.TracerImage(cam, box, acc, grayt.Quality{100})
+	grayt.TracerImage(cam, box, acc, grayt.Quality{spp})
 	img := acc.ToImage(1.0)
 
 	f, err := os.Create(out)
