2d0055c9427e7e923f4482974a328cf79dea7f01
Peter Stace <peterstace@gmail.com>
2016-02-22 21:55:00 +1100

    Make Quality an integer

--- main_helper.go
+++ main_helper.go
@@ -1,6 +1,7 @@
 package grayt
 
 import (
+	"fmt"
 	"image/png"
 	"log"
 	"os"
@@ -10,7 +11,7 @@ import (
 type Runner struct {
 	PxWide, PxHigh int
 	BaseName       string
-	Quality        float64
+	Quality        int
 }
 
 func NewRunner() *Runner {
@@ -27,12 +28,12 @@ func (r *Runner) Run(scene Scene) {
 	world := newWorld(scene.Entities)
 
 	acc := newAccumulator(r.PxWide, r.PxHigh)
-	for i := 0; i < int(r.Quality); i++ {
+	for i := 0; i < r.Quality; i++ {
 		log.Print(i)
 		TraceImage(scene.Camera, world, acc)
 	}
 	img := acc.toImage(1.0) // XXX should be configurable
-	f, err := os.Create(r.BaseName + ".png")
+	f, err := os.Create(r.outputFilename())
 	r.checkErr(err)
 	defer f.Close()
 	err = png.Encode(f, img)
@@ -43,4 +44,9 @@ func (r *Runner) checkErr(err error) {
 	if err != nil {
 		log.Fatal("Fatal: ", err)
 	}
+}
+
+func (r *Runner) outputFilename() string {
+	return fmt.Sprintf("%s_%dx%d_Q%d.png",
+		r.BaseName, r.PxWide, r.PxHigh, r.Quality)
 }
