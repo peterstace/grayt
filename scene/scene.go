3b3c8eaac7db135b55b14c7c778991a0b11e07bf
Peter Stace <peterstace@gmail.com>
2016-10-09 18:10:31 +1100

    Add some more vector operations

--- scene/scene.go
+++ scene/scene.go
@@ -34,6 +34,8 @@ func DefaultCamera() Camera {
 
 type Colour [3]float64
 
+var White = Colour{1, 1, 1}
+
 type Triangle struct {
 	A, B, C   Vector
 	Colour    Colour
@@ -45,12 +47,32 @@ func (t Triangle) String() string {
 		t.A, t.B, t.C, t.Colour, t.Emittance)
 }
 
+func JoinTriangles(ts ...[]Triangle) []Triangle {
+	var all []Triangle
+	for _, t := range ts {
+		all = append(all, t...)
+	}
+	return all
+}
+
 type Vector struct {
 	X, Y, Z float64
 }
 
 func Vect(x, y, z float64) Vector {
 	return Vector{x, y, z}
+}
+
+func (v Vector) Midpoint(u Vector) Vector {
+	return v.Add(u).Scale(0.5)
+}
+
+func (v Vector) Add(u Vector) Vector {
+	return Vector{v.X + u.X, v.Y + u.Y, v.Z + u.Z}
+}
+
+func (v Vector) Scale(f float64) Vector {
+	return Vector{v.X * f, v.Y * f, v.Z * f}
 }
 
 type Scene struct {
