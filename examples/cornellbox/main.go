823b48bfe267742a81d433f2701cb2836592152b
Peter Stace <peterstace@gmail.com>
2016-10-17 19:59:10 +1100

    Make things more fluent

--- examples/cornellbox/main.go
+++ examples/cornellbox/main.go
@@ -27,94 +27,30 @@ func cam() Camera {
 
 func tris() []Triangle {
 	const size = 0.9
-	ts := alignedBox(
-		Vect(size, 1.0, -size),
-		Vect(1.0-size, 0.999, -1.0+size),
-		5.0,
-		White,
+	return JoinTriangles(
+		AlignedBox(
+			Vect(size, 1.0, -size),
+			Vect(1.0-size, 0.999, -1.0+size),
+		).SetColour(White).SetEmittance(5.0),
+		box(),
+		shortBlock(),
+		tallBlock(),
 	)
-	ts = append(ts, box()...)
-	ts = append(ts, shortBlock()...)
-	ts = append(ts, tallBlock()...)
-	return ts
 }
 
-func alignedBox(a, b Vector, emittance float64, colour Colour) []Triangle {
-
-	a1 := Vect(b.X, a.Y, a.Z)
-	a2 := Vect(a.X, b.Y, a.Z)
-	a3 := Vect(a.X, a.Y, b.Z)
-	b1 := Vect(a.X, b.Y, b.Z)
-	b2 := Vect(b.X, a.Y, b.Z)
-	b3 := Vect(b.X, b.Y, a.Z)
-
-	ts := []Triangle{
-
-		{A: a, B: a1, C: a2},
-		{A: a, B: a2, C: a3},
-		{A: a, B: a3, C: a1},
-
-		{A: b, B: b1, C: b2},
-		{A: b, B: b2, C: b3},
-		{A: b, B: b3, C: b1},
-
-		{A: a1, B: b2, C: b3},
-		{A: a2, B: b3, C: b1},
-		{A: a3, B: b1, C: b2},
-
-		{A: b1, B: a2, C: a3},
-		{A: b2, B: a3, C: a1},
-		{A: b3, B: a1, C: a2},
-	}
-
-	for i := range ts {
-		ts[i].Colour = colour
-		ts[i].Emittance = emittance
-	}
-
-	return ts
+func box() TriangleList {
+	return JoinTriangles(
+		JoinTriangles(
+			AlignedSquare(Vect(0, 0, 0), Vect(1, 0, -1)),
+			AlignedSquare(Vect(0, 1, 0), Vect(1, 1, -1)),
+			AlignedSquare(Vect(0, 0, -1), Vect(1, 1, -1)),
+		).SetColour(White),
+		AlignedSquare(Vect(0, 0, 0), Vect(0, 1, -1)).SetColour(Red),
+		AlignedSquare(Vect(1, 0, 0), Vect(1, 1, -1)).SetColour(Green),
+	)
 }
 
-func square(a, b, c, d Vector, emittance float64, colour Colour) []Triangle {
-	return []Triangle{
-		{A: a, B: b, C: c, Emittance: emittance, Colour: colour},
-		{A: c, B: d, C: a, Emittance: emittance, Colour: colour},
-	}
-}
-
-func alignedSquare(a, b Vector, emittance float64, colour Colour) []Triangle {
-	var c, d Vector
-	switch {
-	case a.X == b.X:
-		c = Vector{X: a.X, Y: a.Y, Z: b.Z}
-		d = Vector{X: a.X, Y: b.Y, Z: a.Z}
-	case a.Y == b.Y:
-		c = Vector{X: a.X, Y: a.Y, Z: b.Z}
-		d = Vector{X: b.X, Y: a.Y, Z: a.Z}
-	case a.Z == b.Z:
-		c = Vector{X: a.X, Y: b.Y, Z: a.Z}
-		d = Vector{X: b.X, Y: a.Y, Z: a.Z}
-	default:
-		panic("a and b line in a common aligned plane")
-
-	}
-	return []Triangle{
-		{A: a, B: c, C: d, Colour: colour, Emittance: emittance},
-		{A: b, B: c, C: d, Colour: colour, Emittance: emittance},
-	}
-}
-
-func box() []Triangle {
-	var ts []Triangle
-	ts = append(ts, alignedSquare(Vect(0, 0, 0), Vect(1, 0, -1), 0, White)...)
-	ts = append(ts, alignedSquare(Vect(0, 1, 0), Vect(1, 1, -1), 0, White)...)
-	ts = append(ts, alignedSquare(Vect(0, 0, 0), Vect(0, 1, -1), 0, Red)...)
-	ts = append(ts, alignedSquare(Vect(1, 0, 0), Vect(1, 1, -1), 0, Green)...)
-	ts = append(ts, alignedSquare(Vect(0, 0, -1), Vect(1, 1, -1), 0, White)...)
-	return ts
-}
-
-func shortBlock() []Triangle {
+func shortBlock() TriangleList {
 	var (
 		// Left/Right, Top/Bottom, Front/Back.
 		LBF = Vect(0.76, 0.00, -0.12)
@@ -126,16 +62,16 @@ func shortBlock() []Triangle {
 		RTF = Vect(0.47, 0.30, -0.21)
 		RTB = Vect(0.56, 0.30, -0.49)
 	)
-	var ts []Triangle
-	ts = append(ts, square(LTF, LTB, RTB, RTF, 0, White)...)
-	ts = append(ts, square(LBF, RBF, RTF, LTF, 0, White)...)
-	ts = append(ts, square(LBB, RBB, RTB, LTB, 0, White)...)
-	ts = append(ts, square(LBF, LBB, LTB, LTF, 0, White)...)
-	ts = append(ts, square(RBF, RBB, RTB, RTF, 0, White)...)
-	return ts
+	return JoinTriangles(
+		Square(LTF, LTB, RTB, RTF),
+		Square(LBF, RBF, RTF, LTF),
+		Square(LBB, RBB, RTB, LTB),
+		Square(LBF, LBB, LTB, LTF),
+		Square(RBF, RBB, RTB, RTF),
+	).SetColour(White)
 }
 
-func tallBlock() []Triangle {
+func tallBlock() TriangleList {
 	var (
 		// Left/Right, Top/Bottom, Front/Back.
 		LBF = Vect(0.52, 0.00, -0.54)
@@ -147,11 +83,11 @@ func tallBlock() []Triangle {
 		RTF = Vect(0.23, 0.60, -0.45)
 		RTB = Vect(0.14, 0.60, -0.74)
 	)
-	var ts []Triangle
-	ts = append(ts, square(LTF, LTB, RTB, RTF, 0, White)...)
-	ts = append(ts, square(LBF, RBF, RTF, LTF, 0, White)...)
-	ts = append(ts, square(LBB, RBB, RTB, LTB, 0, White)...)
-	ts = append(ts, square(LBF, LBB, LTB, LTF, 0, White)...)
-	ts = append(ts, square(RBF, RBB, RTB, RTF, 0, White)...)
-	return ts
+	return JoinTriangles(
+		Square(LTF, LTB, RTB, RTF),
+		Square(LBF, RBF, RTF, LTF),
+		Square(LBB, RBB, RTB, LTB),
+		Square(LBF, LBB, LTB, LTF),
+		Square(RBF, RBB, RTB, RTF),
+	).SetColour(White)
 }
--- grayt/api.go
+++ grayt/api.go
@@ -41,12 +41,85 @@ func (t Triangle) String() string {
 		t.A, t.B, t.C, t.Colour, t.Emittance)
 }
 
-func JoinTriangles(ts ...[]Triangle) []Triangle {
+type TriangleList []Triangle
+
+func JoinTriangles(ts ...TriangleList) TriangleList {
 	var all []Triangle
 	for _, t := range ts {
 		all = append(all, t...)
 	}
 	return all
+}
+
+func (t TriangleList) SetColour(c Colour) TriangleList {
+	for i := range t {
+		t[i].Colour = c
+	}
+	return t
+}
+
+func (t TriangleList) SetEmittance(e float64) TriangleList {
+	for i := range t {
+		t[i].Emittance = e
+	}
+	return t
+}
+
+func Square(a, b, c, d Vector) TriangleList {
+	return []Triangle{
+		{A: a, B: b, C: c},
+		{A: c, B: d, C: a},
+	}
+}
+
+func AlignedSquare(a, b Vector) TriangleList {
+	var c, d Vector
+	switch {
+	case a.X == b.X:
+		c = Vector{X: a.X, Y: a.Y, Z: b.Z}
+		d = Vector{X: a.X, Y: b.Y, Z: a.Z}
+	case a.Y == b.Y:
+		c = Vector{X: a.X, Y: a.Y, Z: b.Z}
+		d = Vector{X: b.X, Y: a.Y, Z: a.Z}
+	case a.Z == b.Z:
+		c = Vector{X: a.X, Y: b.Y, Z: a.Z}
+		d = Vector{X: b.X, Y: a.Y, Z: a.Z}
+	default:
+		panic("a and b line in a common aligned plane")
+
+	}
+	return []Triangle{
+		{A: a, B: c, C: d},
+		{A: b, B: c, C: d},
+	}
+}
+
+func AlignedBox(a, b Vector) TriangleList {
+	a1 := Vect(b.X, a.Y, a.Z)
+	a2 := Vect(a.X, b.Y, a.Z)
+	a3 := Vect(a.X, a.Y, b.Z)
+	b1 := Vect(a.X, b.Y, b.Z)
+	b2 := Vect(b.X, a.Y, b.Z)
+	b3 := Vect(b.X, b.Y, a.Z)
+
+	return TriangleList{
+
+		{A: a, B: a1, C: a2},
+		{A: a, B: a2, C: a3},
+		{A: a, B: a3, C: a1},
+
+		{A: b, B: b1, C: b2},
+		{A: b, B: b2, C: b3},
+		{A: b, B: b3, C: b1},
+
+		{A: a1, B: b2, C: b3},
+		{A: a2, B: b3, C: b1},
+		{A: a3, B: b1, C: b2},
+
+		{A: b1, B: a2, C: a3},
+		{A: b2, B: a3, C: a1},
+		{A: b3, B: a1, C: a2},
+	}
 }
 
 type Scene struct {
