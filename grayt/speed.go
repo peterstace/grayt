cd167b929319eda881004dba001c20bd3df9e751
Peter Stace <peterstace@gmail.com>
2017-01-12 20:44:34 +1100

    Bigger image in BenchmarkTraceImage

--- grayt/speed.go
+++ grayt/speed.go
@@ -5,6 +5,6 @@ import "testing"
 func BenchmarkTraceImage(b *testing.B, s Scene) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		TraceImage(16, s, 1, 1, new(uint64))
+		TraceImage(1024, s, 1, 1, new(uint64))
 	}
 }
