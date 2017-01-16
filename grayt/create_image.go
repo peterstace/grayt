f138f10ba2a912cce8ed41485344f1689e90fea4
Peter Stace <peterstace@gmail.com>
2017-01-16 22:52:54 +1100

    Image normalisation refactoring

--- grayt/create_image.go
+++ grayt/create_image.go
@@ -7,43 +7,7 @@ import (
 
 func createImage(a *accumulator) image.Image {
 
-	intensities := make([]float64, len(a.pixels))
-	for i, p := range a.pixels {
-		intensities[i] = intensity(p.colourSum)
-	}
-	sort.Float64s(intensities)
-
-	m := map[float64]float64{}
-	var i int
-	for i < len(intensities) {
-		startI := i
-		for i < len(intensities) && intensities[i] == intensities[startI] {
-			i++
-		}
-		/*
-			x - center
-			min, max
-			top
-
-			c / (top - c) == (c - min) / (max - c)
-			c == (top - c).(c - min)/(max - c)
-			c.(max-c) == (top-c).(c-min)
-			c.max - c*c == top.c - top.min - c*c + c.min
-			c.max == top.c - top.min + c.min
-			c.max - top.c - c.min == -top.min
-			c.(max - top - min) == -top.min
-			c == top.min / (top + min - max)
-		*/
-
-		top := float64(len(intensities))
-		min := float64(startI)
-		max := float64(i)
-		c := top * min / (top + min - max)
-
-		newIntensity := c / float64(len(intensities))
-		oldIntensity := intensities[startI]
-		m[oldIntensity] = newIntensity
-	}
+	intensityMap := buildIntensityMap(a)
 
 	const gamma = 2.2
 	img := image.NewNRGBA(image.Rect(0, 0, a.wide, a.high))
@@ -51,7 +15,7 @@ func createImage(a *accumulator) image.Image {
 		for y := 0; y < a.high; y++ {
 			c := a.get(x, y)
 			oldIntensity := intensity(c)
-			newIntensity, ok := m[oldIntensity]
+			newIntensity, ok := intensityMap[oldIntensity]
 			if !ok {
 				panic(false)
 			}
@@ -65,4 +29,47 @@ func createImage(a *accumulator) image.Image {
 
 func intensity(c Colour) float64 {
 	return 0.2126*c.R + 0.7152*c.G + 0.0722*c.B
+}
+
+func buildIntensityMap(a *accumulator) map[float64]float64 {
+
+	intensities := make([]float64, len(a.pixels))
+	for i, p := range a.pixels {
+		intensities[i] = intensity(p.colourSum)
+	}
+	sort.Float64s(intensities)
+
+	m := map[float64]float64{}
+	var i int
+	for i < len(intensities) {
+		startI := i
+		for i < len(intensities) && intensities[i] == intensities[startI] {
+			i++
+		}
+		center := boxCenter(float64(len(intensities)), float64(startI), float64(i))
+		newIntensity := center / float64(len(intensities))
+		oldIntensity := intensities[startI]
+		m[oldIntensity] = newIntensity
+	}
+
+	return m
+}
+
+func boxCenter(boxMin, boxMax, maxVal float64) float64 {
+	/*
+		x - center
+		min, max
+		top
+
+		c / (top - c) == (c - min) / (max - c)
+		c == (top - c).(c - min)/(max - c)
+		c.(max-c) == (top-c).(c-min)
+		c.max - c*c == top.c - top.min - c*c + c.min
+		c.max == top.c - top.min + c.min
+		c.max - top.c - c.min == -top.min
+		c.(max - top - min) == -top.min
+		c == top.min / (top + min - max)
+	*/
+
+	return maxVal * boxMin / (maxVal + boxMin - boxMax)
 }
