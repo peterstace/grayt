07baf2504ad806bd0fcb35c0440c89c8250c0aa4
Peter Stace <peterstace@gmail.com>
2017-07-25 15:24:48 +1000

    Add reflection system test

--- systemtest/run.sh
+++ systemtest/run.sh
@@ -21,8 +21,9 @@ function run_test()
 }
 
 set -e
-run_test "go run examples/cornellbox/classic/main.go    -w 1024 -q 1"    "classic_single"
-run_test "go run examples/cornellbox/classic/main.go    -w 128  -q 1000" "classic"
-run_test "go run examples/cornellbox/spheretree/main.go -w 256  -q 1"    "sphere_tree_single"
-run_test "go run examples/cornellbox/spheretree/main.go -w 256  -q 100"  "sphere_tree"
-run_test "go run examples/cornellbox/splitbox/main.go   -w 128  -q 50"   "split_box"
+run_test "go run examples/cornellbox/classic/main.go     -w 1024 -q 1"    "classic_single"
+run_test "go run examples/cornellbox/classic/main.go     -w 128  -q 1000" "classic"
+run_test "go run examples/cornellbox/spheretree/main.go  -w 256  -q 1"    "sphere_tree_single"
+run_test "go run examples/cornellbox/spheretree/main.go  -w 256  -q 100"  "sphere_tree"
+run_test "go run examples/cornellbox/splitbox/main.go    -w 128  -q 50"   "split_box"
+run_test "go run examples/cornellbox/reflections/main.go -w 64   -q 1000" "reflection"
