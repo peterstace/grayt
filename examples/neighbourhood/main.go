8679c84edc20d05dc00668789dcaeaa4ec370768
Peter Stace <peter@dgraph.io>
2017-10-16 08:34:00 +1100

    Cylinder demo scene

---
+++ examples/neighbourhood/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"math/rand"
+
+	. "github.com/peterstace/grayt/grayt"
+)
+
+func scene() Scene {
+
+	lightPos := Vect(4, 6, 1)
+	light := Disc(
+		lightPos,
+		3,
+		lightPos.Unit(),
+	).With(Emittance(1.0))
+
+	outerSphere := Sphere(Vector{}, 20).With(Emittance(0.05))
+
+	platform := AlignedBox(
+		Vect(-3, -1, -6),
+		Vect(+1, 0, +2),
+	)
+
+	// Uses space of (-0.5, 0, -0.5) to (0.5, 1, 0.5)
+	placeholder := Sphere(
+		Vect(0, 0.5, 0),
+		0.5,
+	)
+	placeholder = AlignedBox(
+		Vect(-0.5, 0, -0.5),
+		Vect(+0.5, 1, +0.5),
+	)
+	pts := points()
+	placeholder = Group(
+		balls(pts),
+		edges(pts),
+	)
+
+	return Scene{
+		//https://en.wikipedia.org/wiki/Relative_neighborhood_graph
+		Camera: Camera().With(
+			Location(Vect(3, 5, 15)),
+			LookingAt(Vect(0, 0.5, 0)),
+			FieldOfViewInDegrees(6),
+		),
+		Objects: Group(
+			placeholder,
+			platform,
+			outerSphere,
+			light,
+		),
+	}
+}
+
+func edges(pts []Vector) ObjectList {
+	type edge struct {
+		a Vector
+		b Vector
+	}
+	var edges ObjectList
+	for i := range pts {
+		for j := 0; j < i; j++ {
+			ijDist := pts[i].Sub(pts[j]).LengthSq()
+			closer := false
+			for k := range pts {
+				ikDist := pts[i].Sub(pts[k]).LengthSq()
+				jkDist := pts[j].Sub(pts[k]).LengthSq()
+				if ikDist < ijDist && jkDist < ijDist {
+					closer = true
+					break
+				}
+			}
+			if !closer {
+				edges = Group(edges, Pipe(pts[i], pts[j], 0.01))
+			}
+		}
+	}
+	return edges
+}
+
+func balls(pts []Vector) ObjectList {
+	var objs ObjectList
+	for _, p := range pts {
+		objs = Group(objs, Sphere(p, 0.01))
+	}
+	return objs
+}
+
+func points() []Vector {
+	rnd := rand.New(rand.NewSource(1))
+	var pp []Vector
+	for i := 0; i < 1000; i++ {
+		pp = append(pp, Vector{
+			rnd.Float64() - 0.5,
+			rnd.Float64(),
+			rnd.Float64() - 0.5,
+		})
+	}
+	return pp
+}
+
+func main() {
+	Run("neighbourhood", scene())
+}
