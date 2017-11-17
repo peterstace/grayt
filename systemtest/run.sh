7cde51ac519bb699728c5f92e686f8aff18436b0
Peter Stace <peter@dgraph.io>
2017-11-18 10:57:25 +1100

    What version of compare is used?

--- systemtest/run.sh
+++ systemtest/run.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_PATH=$(dirname $(readlink -f $0))
 
+compare --version
+
 function run_test()
 {
   cmd=$1
@@ -22,14 +24,8 @@ function run_test()
 
 set -e
 run_test "go run -race examples/cornellbox/classic/main.go       -d -w 1024 -q 1"    "classic_single"
-echo $?
 run_test "go run       examples/cornellbox/classic/main.go       -d -w 128  -q 1000" "classic"
-echo $?
 run_test "go run       examples/cornellbox/spheretree/main.go    -d -w 256  -q 1"    "sphere_tree_single"
-echo $?
 run_test "go run       examples/cornellbox/spheretree/main.go    -d -w 256  -q 100"  "sphere_tree"
-echo $?
 run_test "go run       examples/cornellbox/splitbox/main.go      -d -w 128  -q 50"   "split_box"
-echo $?
 run_test "go run       examples/cornellbox/reflections/main.go   -d -w 64   -q 1000" "reflection"
-echo $?
