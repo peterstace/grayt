cc83268f8a8f957697b14f1eb89fc46404970c86
Peter Stace <peter@dgraph.io>
2017-11-18 13:09:45 +1100

    Fix script and replace expect images

--- systemtest/run.sh
+++ systemtest/run.sh
@@ -2,8 +2,6 @@
 
 SCRIPT_PATH=$(dirname $(readlink -f $0))
 
-compare --version
-
 function run_test()
 {
   cmd=$1
@@ -12,7 +10,7 @@ function run_test()
   echo -e "\n *** $name ***"
 
   if $cmd -o $SCRIPT_PATH/actual_$name.png &&
-    compare -metric rmse $SCRIPT_PATH/actual_$name.png $SCRIPT_PATH/expect_$name.png null: ; then
+    compare -metric rmse $SCRIPT_PATH/actual_$name.png $SCRIPT_PATH/expect_$name.png null: 2> >(grep "0 (0)") ; then
     echo -e "\nPASSED"
     return 0
   else
@@ -23,9 +21,9 @@ function run_test()
 }
 
 set -e
-run_test "go run -race examples/cornellbox/classic/main.go       -d -w 1024 -q 1"    "classic_single"
-run_test "go run       examples/cornellbox/classic/main.go       -d -w 128  -q 1000" "classic"
-run_test "go run       examples/cornellbox/spheretree/main.go    -d -w 256  -q 1"    "sphere_tree_single"
-run_test "go run       examples/cornellbox/spheretree/main.go    -d -w 256  -q 100"  "sphere_tree"
-run_test "go run       examples/cornellbox/splitbox/main.go      -d -w 128  -q 50"   "split_box"
-run_test "go run       examples/cornellbox/reflections/main.go   -d -w 64   -q 1000" "reflection"
+run_test "go run -race $SCRIPT_PATH/../examples/cornellbox/classic/main.go       -d -w 1024 -q 1"    "classic_single"
+run_test "go run       $SCRIPT_PATH/../examples/cornellbox/classic/main.go       -d -w 128  -q 1000" "classic"
+run_test "go run       $SCRIPT_PATH/../examples/cornellbox/spheretree/main.go    -d -w 256  -q 1"    "sphere_tree_single"
+run_test "go run       $SCRIPT_PATH/../examples/cornellbox/spheretree/main.go    -d -w 256  -q 100"  "sphere_tree"
+run_test "go run       $SCRIPT_PATH/../examples/cornellbox/splitbox/main.go      -d -w 128  -q 50"   "split_box"
+run_test "go run       $SCRIPT_PATH/../examples/cornellbox/reflections/main.go   -d -w 64   -q 1000" "reflection"
