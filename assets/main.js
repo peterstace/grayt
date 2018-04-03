500e0072ed5be8b530dd25e055a50666bbe3a9e3
Peter Stace <peter@spaceship.com.au>
2018-04-03 20:31:49 +1000

    List resources. Button to create new resource.

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,23 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'http://localhost:6060/renders', true);
-request.onload = function () {
-  var data = JSON.parse(this.response);
-  console.log(data);
+document.getElementById("new-resource-button").addEventListener("click", function() {
+  var request = new XMLHttpRequest();
+  request.open('POST', 'http://localhost:6060/renders');
+  request.send();
+  updateResourceList();
+});
+
+function updateResourceList() {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://localhost:6060/renders');
+  request.onload = function () {
+    var data = JSON.parse(this.response);
+    document.getElementById("resource-header").innerHTML = "Resources (" + data.length + ")";
+    var resourceList = document.getElementById("resource-list");
+    resourceList.innerHTML = "";
+    for (var i = 0; i < data.length; i++) {
+      resourceList.innerHTML += "<li>" + data[i] + "</li>";
+    }
+  }
+  request.send();
 }
-request.send();
+
+updateResourceList();
