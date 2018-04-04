526321ff224feb6ebb90fbf834eb69f8c32361a6
Peter Stace <peter@spaceship.com.au>
2018-04-05 07:56:37 +1000

    Start of selecting active resource

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,10 @@
+var activeUuid = '';
+
 document.getElementById("new-resource-button").addEventListener("click", function() {
   var request = new XMLHttpRequest();
   request.open('POST', 'http://localhost:6060/renders');
+  request.onload = updateResourceList;
   request.send();
-  updateResourceList();
 });
 
 function updateResourceList() {
@@ -14,8 +16,13 @@ function updateResourceList() {
     var resourceList = document.getElementById("resource-list");
     resourceList.innerHTML = "";
     for (var i = 0; i < data.length; i++) {
-      resourceList.innerHTML += "<li>" + data[i] + "</li>";
+      if (data[i] == activeUuid) {
+        resourceList.innerHTML += '<li>Selected: ' + data[i] + "</li>"; // TODO: Use a CSS class and highlight.
+      } else {
+        resourceList.innerHTML += "<li>" + data[i] + "</li>";
+      }
     }
+    // TODO: Add an event to select the UUID.
   }
   request.send();
 }
