acd2bc74da8585b101b1682853cb5fac1b36ef6e
Peter Stace <peter@spaceship.com.au>
2018-04-07 14:28:05 +1000

    Show image

--- assets/main.js
+++ assets/main.js
@@ -1,43 +1,57 @@
 var activeUuid = '';
 
 document.getElementById("new-resource-button").addEventListener("click", function() {
-  var request = new XMLHttpRequest();
+  let request = new XMLHttpRequest();
   request.open('POST', 'http://localhost:6060/renders');
   request.onload = updateResourceList;
   request.send();
 });
 
 function updateResourceList() {
-  var request = new XMLHttpRequest();
+  let request = new XMLHttpRequest();
   request.open('GET', 'http://localhost:6060/renders');
   request.onload = function () {
 
-    var mainWin = document.getElementById("main-window");
-    mainWin.innerHTML = '';
+    // TODO: Remove event handlers.
+    // TODO: Gray out inputs and buttons if nothing active.
     if (activeUuid != '') {
-      mainWin.innerHTML += '<input id="input-scene-name" type="text"/>';
-      mainWin.innerHTML += '<button id="post-scene-name">POST Scene Name</button>';
-      document.getElementById("post-scene-name").addEventListener("click", function() {
-        // TODO: Send a post
-        //var addr = 'localhost:6060/renders/' + activeUuid + '/scene'
-        //alert(document.getElementById("input-scene-name").value);
-        //alert(addr);
-      });
-    }
 
-    var data = JSON.parse(this.response);
+      document.getElementById("put-scene-name").addEventListener("click", function() {
+        let request = new XMLHttpRequest();
+        let url  = 'http://localhost:6060/renders/' + activeUuid + '/scene';
+        request.open('PUT', url);
+        request.onload = function() {
+          alert('Status: ' + this.status + '\nResponse: ' + this.response);
+        };
+        request.send(document.getElementById('input-scene-name').value);
+      });
+      
+      document.getElementById('put-running').addEventListener("click", function() {
+        let request = new XMLHttpRequest();
+        let url  = 'http://localhost:6060/renders/' + activeUuid + '/running';
+        request.open('PUT', url);
+        request.onload = function() {
+          alert('Response: ' + this.response);
+        };
+        request.send('true');
+      });
+
+       document.getElementById('img').setAttribute('src', 'http://localhost:6060/renders/' + activeUuid + '/image');
+    };
+
+    let data = JSON.parse(this.response);
     document.getElementById("resource-header").innerHTML = "Resources (" + data.length + ")";
-    var resourceList = document.getElementById("resource-list");
+    let resourceList = document.getElementById("resource-list");
     resourceList.innerHTML = "";
-    for (var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       if (data[i] == activeUuid) {
         resourceList.innerHTML += '<li class="selected">' + data[i] + "</li>";
       } else {
         resourceList.innerHTML += "<li>" + data[i] + "</li>";
       }
     }
-    var listItems = resourceList.childNodes;
-    for (var i = 0; i < listItems.length; i++) {
+    let listItems = resourceList.childNodes;
+    for (let i = 0; i < listItems.length; i++) {
       listItems[i].addEventListener("click", function() {
         activeUuid = this.innerHTML;
         updateResourceList();
