c280d541927cf159e37ba7538b97f0fb3b155333
Peter Stace <peter@spaceship.com.au>
2018-04-15 19:21:43 +1000

    Use table for adding new resource

--- assets/main.js
+++ assets/main.js
@@ -69,7 +69,7 @@ function populateResolutionCheckboxes() {
       for (let j = 0; j < xWides.length; j++) {
         let pxWide = xWides[j];
         let pxHigh = scene.aspect_high * pxWide / scene.aspect_wide;
-        inner += '<input type="checkbox">' + pxWide + 'x' + pxHigh + '</input><br/>';
+        inner += '<option value="' + pxWide + '">' + pxWide + 'x' + pxHigh + '</option>';
       }
       resolutionsDiv.innerHTML = inner;
       return;
